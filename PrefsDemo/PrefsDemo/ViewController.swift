c9c80086804c7578eb732cb105cd72ee632264b4
Ted Neward <ted@tedneward.com>
2024-05-10 14:14:43 -0700

    Updating PrefsDemo with new propertylist examples

--- PrefsDemo/PrefsDemo/ViewController.swift
+++ PrefsDemo/PrefsDemo/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 struct Person: Codable {
     var name: String
 }
+struct Values: Codable {
+    var webserviceURL:String = ""
+    var itemsPerPage:Int = -1
+    var backupEnabled:Bool = false
+}
+
 
 class ViewController: UIViewController {
 
@@ -18,20 +24,34 @@ class ViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
+        // Let's delete any raw plist file already present.
+        let valuespath = NSHomeDirectory() + "/Documents/values.plist"
+        let filemanager = FileManager.default
+        if filemanager.fileExists(atPath: valuespath) {
+            // If this can't delete, something is really wrong.
+            // So just force the call and crash if it throws.
+            try! FileManager.default.removeItem(atPath: valuespath)
+        }
     }
     
     @IBAction func btnLoadPressed(_ sender: Any) { loadPrefs() }
     
     @IBAction func btnSavePressed(_ sender: Any) { savePrefs() }
     
+    @IBAction func btnRawSavePressed(_ sender: Any) { saveRaw() }
+    
+    @IBAction func btnRawLoadPressed(_ sender: Any) { loadRaw() }
+    
+    // {{## BEGIN save-settings ##}}
     func savePrefs() {
         let defaults = UserDefaults.standard
         
-        defaults.set(25, forKey: "Age")
-        defaults.set(true, forKey: "UseTouchID")
-        defaults.set(CGFloat.pi, forKey: "Pi")
-
-        defaults.set("Paul Hudson", forKey: "Name")
+        defaults.set("Paul Hudson", forKey: "name")
+        defaults.set(25, forKey: "age")
+        defaults.set(true, forKey: "useTouchID")
+        defaults.set(CGFloat.pi, forKey: "pi")
+        
+        // These don't appear in Settings but are still stored
         defaults.set(Date.now, forKey: "LastRun")
 
         let array = ["Hello", "World"]
@@ -41,34 +61,48 @@ class ViewController: UIViewController {
         defaults.set(dict, forKey: "SavedDict")
         
         let taylor = Person(name: "Taylor Swift")
-        let encoder = JSONEncoder()
-        if let encoded = try? encoder.encode(taylor) {
+        if let encoded = try? JSONEncoder().encode(taylor) {
             defaults.set(encoded, forKey: "SavedPerson")
         }
     }
-    
+    // {{## END save-settings ##}}
+    // {{## BEGIN save-raw-plist ##}}
+    func saveRaw() {
+        // Let's try writing to a new plist file
+        let values = Values(webserviceURL: "http://this.doesnt.exist.com",
+                            itemsPerPage: 10,
+                            backupEnabled : true)
+        print("Encoding values:", values)
+        
+        let encoder = PropertyListEncoder()
+        encoder.outputFormat = .xml
+
+        let valuespath = URL(filePath: NSHomeDirectory() + "/Documents/values.plist")
+        do {
+            let data = try encoder.encode(values)
+            try data.write(to: valuespath)
+        } catch {
+            print(error)
+        }
+    }
+    // {{## END save-raw-plist ##}}
+
+    // {{## BEGIN load-settings ##}}
     func loadPrefs() {
         let defaults = UserDefaults.standard
         
-        print(defaults.integer(forKey: "Age"))
-        print(defaults.bool(forKey: "UseTouchID"))
-        print(defaults.float(forKey: "Pi"))
+        // Note that only some of these will appear in the
+        // Settings app; in fact, only the ones specified
+        // in the "Settings.bundle/Root.plist" will show
+        // up in Settings.
+        if let name = defaults.string(forKey: "name") { print(name) }
+        print(defaults.integer(forKey: "age"))
+        print(defaults.bool(forKey: "useTouchID"))
+        print(defaults.float(forKey: "pi"))
 
-        if let name = defaults.string(forKey: "Name") {
-            print(name)
-        }
-        if let lastRun = defaults.object(forKey: "LastRun") {
-            print(lastRun)
-        }
-
-        if let array = defaults.array(forKey: "SavedArray") {
-            print(array)
-        }
-
-        if let dict = defaults.dictionary(forKey: "SavedDict") {
-            print(dict)
-        }
-
+        if let lastRun = defaults.object(forKey: "LastRun") { print(lastRun) }
+        if let array = defaults.array(forKey: "SavedArray") { print(array) }
+        if let dict = defaults.dictionary(forKey: "SavedDict") { print(dict) }
         if let savedPerson = defaults.object(forKey: "SavedPerson") as? Data {
             let decoder = JSONDecoder()
             if let loadedPerson = try? decoder.decode(Person.self, from: savedPerson) {
@@ -76,6 +110,19 @@ class ViewController: UIViewController {
             }
         }
     }
-
+    // {{## END load-settings ##}}
+    // {{## BEGIN load-raw-plist ##}}
+    func loadRaw() {
+        let decoder = PropertyListDecoder()
+        let valuespath = URL(filePath: NSHomeDirectory() + "/Documents/values.plist")
+        do {
+            let data = try Data(contentsOf: valuespath)
+            let values = try decoder.decode(Values.self, from: data)
+            print("Decoded values: ", values)
+        } catch {
+            print(error)
+        }
+    }
+    // {{## END load-raw-plist ##}}
 }
 
