e82e048ecdcfe1ffa3aa60e007d9ee10a3c6bdbb
Ted Neward <ted@tedneward.com>
2025-05-06 15:44:14 -0700

    Filling in some gaps

---
+++ Firebase/FirebaseData/FirebaseData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseData
+//
+//  Created by Ted Neward on 5/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
